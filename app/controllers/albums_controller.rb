e1b29d3ce96bfd8dcf115c14c97094f0cf477902
mizech <michael.zech@hotmail.de>
2025-10-15 17:02:29 +0200

    Impl.: Artist#new

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,4 +2,24 @@ class AlbumsController < ApplicationController
   def index
     @albums = Album.all
   end
+
+  def new
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(album_params)
+
+    if @album.save
+      redirect_to albums_path, notice: "Album was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def album_params
+    params.expect(album: [ :name, :price, :artist_id ])
+  end
 end
