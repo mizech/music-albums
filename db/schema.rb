18fe4eb00840f64741e4a861940d7ac10cdec67f
mizech <michael.zech@hotmail.de>
2025-11-02 10:13:18 +0100

    Added: Devise-gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_16_080147) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_02_091033) do
   create_table "albums", force: :cascade do |t|
     t.string "name"
     t.float "price"
@@ -35,6 +35,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_16_080147) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["album_id"], name: "index_comments_on_album_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "albums", "artists"
